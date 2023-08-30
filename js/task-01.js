e1b8260c0878445b8a65d5625886525a6eb99842
Myshkina-Alla <myshkina.alla@icloud.com>
2023-08-30 18:50:00 +0300

    task 1-3

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+// const container = document.querySelector("#categories");
+// const category = container.querySelectorAll(".item");
+// console.log(`Number of categories: ${category.length}`);
+// category.forEach((item) => {
+//   const categoryItem = item.querySelector("h2").textContent;
+//   const categoryElement = item.querySelectorAll("li").length;
+//   console.log(`Category: ${categoryItem}`);
+//   console.log(`Elements: ${categoryElement}`);
+// });
---
+++ js/task-02.js
@@ -0,0 +1,17 @@
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+const container = document.querySelector("#ingredients");
+const markup = ingredients.map((item) => {
+  const liElements = document.createElement("li");
+  liElements.textContent = item;
+  liElements.classList = "item";
+  return liElements;
+});
+container.append(...markup);
+console.log(container);
